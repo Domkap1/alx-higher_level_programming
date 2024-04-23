34c1698a3b10dca65c70cb349e834fa2b586162b
Domkap1 <domkappaul@gmail.com>
2024-04-23 21:12:50 +0100

    corrects

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,11 @@
-#!/usr/bin/node
 // computes and prints a factorial
 
-const factorial = n => isNaN(n) ? 1 : (n <= 1 ? 1 : n * factorial(n - 1));
+function factorial (n) {
+  if ((isNaN(n)) || (n === 1)) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
 
 console.log(factorial(parseInt(process.argv[2])));
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 // prints the addition of 2 integers
 
-function add(a, b) {
-	return a + b;
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
 
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-if (isNaN(num1) || isNaN(num2)) {
-	console.log('NaN,');
-} else {
-	console.log(add(num1, num2));
-}
+console.log(add(process.argv[2], process.argv[3]));
