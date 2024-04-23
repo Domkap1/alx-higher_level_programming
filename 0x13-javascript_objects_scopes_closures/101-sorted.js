9bbc799592e5896a00e71df83995ce9d3c510c25
Domkap1 <domkappaul@gmail.com>
2024-04-23 20:36:36 +0100

    crect

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+const { dict } = require('./101-data.js');
 let newDict = {};
 for (let key in dict) {
 	if (!newDict[dict[key]]) {
 		newDict[dict[key]] = [];
-  }
-  newDict[dict[key]].push(parseInt(key));
+	}
+	newDict[dict[key]].push(parseInt(key));
 }
 console.log(newDict);
-
