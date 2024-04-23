8b0832d54049b83b0d7fca454795290373295771
Domkap1 <domkappaul@gmail.com>
2024-04-23 21:03:50 +0100

    8square

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
-
-const lang = 'C is fun';
+// prints a square
 
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log(lang);
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
