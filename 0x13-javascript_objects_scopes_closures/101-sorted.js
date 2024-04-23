66f14ebac39ff84bba2a47c2218765d70b13ec04
Domkap1 <domkappaul@gmail.com>
2024-04-23 20:30:14 +0100

    sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,10 @@
 const dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+	if (!newDict[dict[key]]) {
+		newDict[dict[key]] = [];
   }
+  newDict[dict[key]].push(parseInt(key));
 }
 console.log(newDict);
+
