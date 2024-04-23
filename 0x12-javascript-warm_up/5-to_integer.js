bbc1f103e9e4f7c71887f63423271ff3c291d541
Domkap1 <domkappaul@gmail.com>
2024-04-23 20:47:44 +0100

    integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 // prints two arguments passed to it, in the following format: “ is ”
 
-const number = parseInt(process.argv[2]);
-
-if (!isNaN(number)) {
-	console.log('My number:', number);
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
 } else {
-	console.log('Not a number');
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
