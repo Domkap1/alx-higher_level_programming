3d8dac8503ca5cd3fb7996e5ebddc1416ef9666a
Domkap1 <domkappaul@gmail.com>
2024-05-10 08:08:34 +0100

    1-body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o /dev/null -w "%{http_code}" "$1" | xargs -I {} sh -c 'if [ "{}" -eq 200 ]; then curl -s "$1"; fi' dummy
+curl -s -o /dev/null -w "%{http_code}" "$1" | xargs -I {} sh -c 'if [ "{}" -eq 200 ]; then curl -s "$1"; fi' dummy | grep "Route 2" | awk '{print $3}'
