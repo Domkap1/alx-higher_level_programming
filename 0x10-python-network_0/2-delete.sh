50bdb62443836fea8f8c7628a89f459a2b6af681
Domkap1 <domkappaul@gmail.com>
2024-05-10 02:22:06 -0700

    1-body

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s "$1" -X DELETE
+curl -s -o /dev/null -w "%{http_code}" -X DELETE "$1" | { read status_code; if [ "$status_code" -eq 200 ]; then echo "route accept DELETE method"; else echo "route doesn’t accept DELETE method"; fi; }
