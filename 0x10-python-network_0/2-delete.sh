2324c5ca12b220df123f7ee66c9cb59b0228ec39
Domkap1 <111006017+Domkap1@users.noreply.github.com>
2024-06-14 11:11:20 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/2-delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-<<<<<<< HEAD
-curl -s -X DELETE "$1"
-=======
-curl -s -o /dev/null -w "%{http_code}" -X DELETE "$1" | { read status_code; if [ "$status_code" -eq 200 ]; then echo "route accept DELETE method"; else echo "route doesn’t accept DELETE method"; fi; }
->>>>>>> 50bdb62443836fea8f8c7628a89f459a2b6af681
