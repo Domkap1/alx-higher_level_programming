7f91ff9eb378ab76fda4fe8105ad58d288872bac
Domkap1 <domkappaul@gmail.com>
2024-06-20 01:18:38 -0700

    correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // searches the second biggest integer in the list of arguments.
 
-if (process.argv.length <= 4) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const list = process.argv.slice(2).map(number).sort((a, b) => b - a);
+  console.log(list[1]);
 }
