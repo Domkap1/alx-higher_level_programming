2b8a80c0de94c61d28993ffd6c51b3045fec52ab
Domkap1 <domkappaul@gmail.com>
2024-06-20 11:53:58 -0700

    concat

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-    return function convert(number) {
-        if (number === 0) return '0';
-        function recurse(n) {
-            return n === 0 ? '' : recurse(Math.floor(n / base)) + (n % base).toString(base);
-        }
-        return recurse(number);
+  return function convert(number) {
+  if (number === 0) return '0';
+    function recurse(n) {
+      return n === 0 ? '' : recurse(Math.floor(n / base)) + (n % base).toString(base);
     }
+    return recurse(number);
+  }
 }
