80eec0bf127b2c5fbfa1347519545c4549f6408a
Domkap1 <domkappaul@gmail.com>
2024-06-24 11:40:09 -0700

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,16 +14,20 @@ if __name__ == '__main__':
     Access the database and get the states
     from the database.
     """
+    # Connect to the database
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
 
+    # Create query using format
     query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4])
     db_cursor.execute(query)
     rows_selected = db_cursor.fetchall()
 
+    # Print each row
     for row in rows_selected:
         print(row)
 
+    # Close cursor and connection
     db_cursor.close()
     db_connect.close()
