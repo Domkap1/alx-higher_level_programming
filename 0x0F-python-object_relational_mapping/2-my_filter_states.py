8fa8e7c5f0da0ee99b00a8ba29ef2f44609baff3
Domkap1 <domkappaul@gmail.com>
2024-06-24 11:28:33 -0700

    ORMCC

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,16 +11,15 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the states
+    Access the database and get the states
     from the database.
     """
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                        states.id ASC".format(argv[4]))
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    db_cursor.execute(query, (argv[4],))
     rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
